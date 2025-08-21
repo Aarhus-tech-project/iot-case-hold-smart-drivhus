898ac7c9a08f4ec64c0d8111e2dbd270b5fdc0cc
JonasEHartig <jonashartige@gmail.com>
2025-08-21 11:07:00 +0200

    ESP8266WiFi initial setup, connected to wifi

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,11 +1,32 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
-// put function declarations here:
+const char* ssid     = "h4prog";      
+const char* password = "1234567890";
+
 int myFunction(int, int);
 
+void ConnectWifi()
+{
+  Serial.println();
+  Serial.println("Connecting to WiFi...");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected!\n IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(10);
+
+  ConnectWifi();
 }
 
 void loop() {
