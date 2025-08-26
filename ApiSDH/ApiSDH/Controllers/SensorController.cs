3bca39a87335e58d0ec17b1b7cc351d1195f1a8f
NP <np@skoleit.dk>
2025-08-26 09:23:43 +0200

    add get sensor readings query.

--- ApiSDH/ApiSDH/Controllers/SensorController.cs
+++ ApiSDH/ApiSDH/Controllers/SensorController.cs
@@ -1,5 +1,6 @@
 ﻿using ApiSDH.Common.Interfaces.Factories;
 using Application.SensorReadings.Commands.Create;
+using Application.SensorReadings.Queries.GetList;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,13 @@ public class SensorController(IMediator mediator, IResponseFactory responseFacto
     public async Task<IActionResult> CreateSensorData([FromBody] CreateSensorReadingCommand command)
     {
         var result = await mediator.Send(command);
+        return responseFactory.CreateResponse(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetSensorData() // Måske ikke fra route eftersom at der ikke kommer noget 
+    {
+        var result = await mediator.Send(new GetSensorReadingsQuery());
         return responseFactory.CreateResponse(result);
     }
 }
---
+++ ApiSDH/Application/SensorReadings/Queries/GetList/GetSensorReadingsQuery.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces.Factories;
+using Application.Common.Interfaces.Persistence;
+using Application.Common.Models;
+using Application.SensorReadings.Dtos;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.SensorReadings.Queries.GetList;
+
+public record GetSensorReadingsQuery : IRequest<Result<List<SensorReadingDto>>>;
+
+public class GetSensorReadingsQueryHandler(ISensorContext sensorContext, IResultFactory resultFactory, IMapper mapper)
+    : IRequestHandler<GetSensorReadingsQuery, Result<List<SensorReadingDto>>>
+{
+    public async Task<Result<List<SensorReadingDto>>> Handle(GetSensorReadingsQuery request,
+        CancellationToken cancellationToken)
+    {
+        // order by newest 
+        var listToReturn = await sensorContext.SensorReadings.AsNoTracking().Take(200)
+            .OrderByDescending(r => r.CreatedAt)
+            .ToListAsync(cancellationToken);
+
+        if (listToReturn.Count is 0) return resultFactory.NotFound<List<SensorReadingDto>>();
+
+        return resultFactory.Ok(mapper.Map<List<SensorReadingDto>>(listToReturn));
+    }
+}
