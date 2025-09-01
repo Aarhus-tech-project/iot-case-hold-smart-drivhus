11b83f8228fe46458879a1a890afc0d5fd14dd5c
JonasEHartig <jonashartige@gmail.com>
2025-09-01 13:26:45 +0200

    Arduino sensor; CCS811 Air Quality, BME280 sparkfun atmospheric sensor, HW-103 og HW-080 jord fugtigheds sensor og TSL2591 light sensor kode

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,39 +1,241 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
+#include "Adafruit_CCS811.h"
 
-const char* ssid     = "h4prog";      
-const char* password = "1234567890";
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
 
-int myFunction(int, int);
+#define ledPin 6
+#define sensorPin A0
 
-void ConnectWifi()
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
+Adafruit_BME280 bme; 
+Adafruit_CCS811 ccs;
+
+unsigned long delayTime;
+
+void displaySensorDetails(void)
 {
-  Serial.println();
-  Serial.println("Connecting to WiFi...");
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" lux"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" lux"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution, 4); Serial.println(F(" lux"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  delay(500);
+}
 
-  WiFi.begin(ssid, password);
+void configureSensor(void)
+{
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+  
+  // Changing the integration time gives you a longer time over which to sense light
+  // longer timelines are slower, but are good in very low light situtations!
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Gain:         "));
+  tsl2591Gain_t gain = tsl.getGain();
+  switch(gain)
+  {
+    case TSL2591_GAIN_LOW:
+      Serial.println(F("1x (Low)"));
+      break;
+    case TSL2591_GAIN_MED:
+      Serial.println(F("25x (Medium)"));
+      break;
+    case TSL2591_GAIN_HIGH:
+      Serial.println(F("428x (High)"));
+      break;
+    case TSL2591_GAIN_MAX:
+      Serial.println(F("9876x (Max)"));
+      break;
+  }
+  Serial.print  (F("Timing:       "));
+  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
+  Serial.println(F(" ms"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+}
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println(F("Starting Adafruit TSL2591 Test!"));
+  
+  if (tsl.begin()) 
+  {
+    Serial.println(F("Found a TSL2591 sensor"));
+  } 
+  else 
+  {
+    Serial.println(F("No sensor found ... check your wiring?"));
+    while (1);
+  }
+    
+  displaySensorDetails();
+  configureSensor();
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  unsigned status;
+
+  status = bme.begin();  
+    // You can also pass in a Wire library object like &Wire2
+    // status = bme.begin(0x76, &Wire2)
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+        Serial.print("        ID of 0x60 represents a BME 280.\n");
+        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        while (1) delay(10);
+    }
+    
+    Serial.println("-- Default Test --");
+    delayTime = 1000;
+
+    Serial.println();
+
+    Serial.println("CCS811 test");
+
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
   }
 
-  Serial.println("\nWiFi connected!\n IP address: ");
-  Serial.println(WiFi.localIP());
+  // Wait for the sensor to be ready
+  while(!ccs.available());
+
 }
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
+void simpleRead(void)
+{
+  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
+  //uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
+  //uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
 
-  ConnectWifi();
+  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+  Serial.print(F("Luminosity: "));
+  Serial.println(x, DEC);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void advancedRead(void)
+{
+  uint32_t lum = tsl.getFullLuminosity();
+  uint16_t ir, full;
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
+  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
+  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
+  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
+  Serial.println("");
+  delay(500);
+  // simpleRead(); 
+  // unifiedSensorAPIRead();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void unifiedSensorAPIRead(void)
+{
+  sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  Serial.print(F("[ ")); Serial.print(event.timestamp); Serial.print(F(" ms ] "));
+  if ((event.light == 0) |
+      (event.light > 4294966000.0) | 
+      (event.light <-4294966000.0))
+  {
+    Serial.println(F("Invalid data (adjust gain or timing)"));
+  }
+  else
+  {
+    Serial.print(event.light); Serial.println(F(" lux"));
+  }
+}
+
+void readSensor() {
+  int sensorValue = analogRead(sensorPin);  // Read the analog value from sensor
+  int outputValue = map(sensorValue, 0, 1023, 255, 0); // map the 10-bit data to 8-bit data
+  analogWrite(ledPin, outputValue); // generate PWM signal
+  Serial.print("Analog output: ");
+  Serial.println(outputValue);
+  Serial.println("");
+  delay(500);
+  //return outputValue;             // Return analog moisture value
+}
+
+void printValues() {
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" °C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+    delay(500);
+}
+
+void printCss()
+  {
+     if(ccs.available()){
+    if(!ccs.readData()){
+      Serial.print("CO2: ");
+      Serial.print(ccs.geteCO2());
+      Serial.print("ppm, TVOC: ");
+      Serial.println(ccs.getTVOC());
+      Serial.println("");
+    }
+    else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+    }
+    delay(150);
+  }
+
+
+void loop(void) 
+{  
+  advancedRead();
+
+  readSensor();
+
+  printValues();
+
+  printCss();
 }
