591c09629a3084387b56885ed4be7ef98d569490
Np <np@skoleit.dk>
2025-09-12 08:53:17 +0200

    Status service azure fix.

--- ApiSDH/Infrastructure/InterfaceImplementations/Services/StatusService.cs
+++ ApiSDH/Infrastructure/InterfaceImplementations/Services/StatusService.cs
@@ -5,20 +5,26 @@ namespace Infrastructure.InterfaceImplementations.Services;
 public class StatusService : IStatusService
 {
     // Could come from config
-    private readonly string _statusFolder = Path.Combine("logs", "status");
+    private static string StatusFolder;
 
     public StatusService()
     {
-        if (!Directory.Exists(_statusFolder))
-            Directory.CreateDirectory(_statusFolder);
+        //if (!Directory.Exists(_statusFolder))
+          //  Directory.CreateDirectory(_statusFolder);
+          // In Azure, HOME points to D:\home
+          var logRoot = Environment.GetEnvironmentVariable("HOME")
+                        ?? AppContext.BaseDirectory;
+
+          StatusFolder = Path.Combine(logRoot, "LogFiles", "status");
+          Directory.CreateDirectory(StatusFolder);
     }
 
     public void Write(string message)
     {
-        var fileName = $"Status{DateTime.Now:yyyy-MM-dd}.txt";
-        var filePath = Path.Combine(_statusFolder, fileName);
+        string fileName = $"Status{DateTime.Now:yyyy-MM-dd}.txt";
+        string filePath = Path.Combine(StatusFolder, fileName);
 
-        var logEntry = $"{DateTime.Now:HH:mm:ss} - {message}{Environment.NewLine}";
+        string logEntry = $"{DateTime.Now:HH:mm:ss} - {message}{Environment.NewLine}";
         File.AppendAllText(filePath, logEntry);
     }
 }
